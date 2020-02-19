848393d10cf08631ad1ae85bcfa7ae44cd226be7
Hamish Knight <hamishknight@posteo.net>
2020-02-19 13:22:25 -0800

    xxx

--- lib/ClangImporter/ImporterImpl.h
+++ lib/ClangImporter/ImporterImpl.h
@@ -528,14 +528,9 @@ private:
   /// NSObject, imported into Swift.
   Type NSObjectTy;
 
-  /// A pair containing a ClangModuleUnit,
-  /// and whether the overlays of its re-exported modules have all been forced
-  /// to load already.
-  using ModuleInitPair = llvm::PointerIntPair<ClangModuleUnit *, 1, bool>;
-
 public:
   /// A map from Clang modules to their Swift wrapper modules.
-  llvm::SmallDenseMap<const clang::Module *, ModuleInitPair, 16> ModuleWrappers;
+  llvm::SmallDenseMap<const clang::Module *, ClangModuleUnit *, 16> ModuleWrappers;
 
   /// The module unit that contains declarations from imported headers.
   ClangModuleUnit *ImportedHeaderUnit = nullptr;
@@ -920,12 +915,12 @@ public:
 
   /// Retrieves the Swift wrapper for the given Clang module, creating
   /// it if necessary.
-  ClangModuleUnit *getWrapperForModule(const clang::Module *underlying);
+  ClangModuleUnit *getWrapperForModule(const clang::Module *underlying,
+                                       SourceLoc importLoc = SourceLoc());
 
   /// Constructs a Swift module for the given Clang module.
   ModuleDecl *finishLoadingClangModule(SourceLoc importLoc,
-                                       const clang::Module *clangModule,
-                                       bool preferOverlay);
+                                       const clang::Module *clangModule);
 
   /// Call finishLoadingClangModule on each deferred import collected
   /// while scanning a bridging header or PCH.
