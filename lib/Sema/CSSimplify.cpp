a28cfbd4f751c1ac5ba4457ce5792bdffba6e578
Hamish Knight <hamish_github@mediocremail.com>
2023-04-18 19:25:28 +0100

    [CS] Fix a couple of constraints in `getTypeForPattern`
    The TypedPattern and IsPattern constraints were
    incorrectly written, with conversions propagating
    out of the patterns, when really conversions
    ought to propagate into patterns. In any case, it
    seems like we really want equality here. Fix the
    constraints to use equality, and have the cast
    constraint operate on the external pattern type
    instead of the subpattern type.

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -14732,8 +14732,7 @@ ConstraintSystem::SolutionKind ConstraintSystem::simplifyFixConstraint(
     if (recordFix(fix))
       return SolutionKind::Error;
 
-    (void)matchTypes(type1, OptionalType::get(type2),
-                     ConstraintKind::Conversion,
+    (void)matchTypes(type1, OptionalType::get(type2), ConstraintKind::Equal,
                      TypeMatchFlags::TMF_ApplyingFix, locator);
 
     return SolutionKind::Solved;
