c9dae77b2dfdd74a76bff4d86b14ddef3b4ad216
Hamish Knight <hamish_github@mediocremail.com>
2024-09-22 20:43:50 +0100

    Remove merge-module test in `gen_module_group.swift`
    Only test using the separate module job.

--- test/SourceKit/InterfaceGen/gen_module_group.swift
+++ test/SourceKit/InterfaceGen/gen_module_group.swift
@@ -3,11 +3,10 @@
 // RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name A -- -I %t.mod -target %target-triple | %FileCheck -check-prefix=GROUPA %s
 // RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name B -- -I %t.mod -target %target-triple | %FileCheck -check-prefix=GROUPB %s
 
-// FIXME: We don't currently handle group info for multi-file builds,
-// so just make sure we don't crash.
-// RUN: %empty-directory(%t.multifrontend)
-// RUN: %target-build-swift -module-name MyModule -emit-module -emit-module-path %t.multifrontend/MyModule.swiftmodule -no-emit-module-separately -Xfrontend -group-info-path -Xfrontend %S/Inputs/group.json %S/Inputs/swift_mod.swift %S/Inputs/swift_mod_syn.swift
-// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name A -- -I %t.multifrontend -target %target-triple | %FileCheck -check-prefix=EMPTY %s
+// RUN: %empty-directory(%t.mod)
+// RUN: %target-build-swift -module-name MyModule -emit-module -emit-module-path %t.mod/MyModule.swiftmodule -Xfrontend -group-info-path -Xfrontend %S/Inputs/group.json %S/Inputs/swift_mod.swift %S/Inputs/swift_mod_syn.swift
+// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name A -- -I %t.mod -target %target-triple | %FileCheck -check-prefix=GROUPA %s
+// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name B -- -I %t.mod -target %target-triple | %FileCheck -check-prefix=GROUPB %s
 
 // GROUPA: MyClass
 // GROUPA-NOT: P1
@@ -16,7 +15,3 @@
 // GROUPB: P1
 // GROUPB: P2
 // GROUPB-NOT: MyClass
-
-// EMPTY-NOT: P1
-// EMPTY-NOT: P2
-// EMPTY-NOT: MyClass
